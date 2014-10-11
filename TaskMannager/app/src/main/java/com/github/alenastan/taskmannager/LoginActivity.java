26715f1a344afe33b0682f0caaadfa7ffca71c71
Alena Stanchyts <lenka_stan@mail.ru>
2014-10-11 18:58:57 +0300

    added activity

---
+++ TaskMannager/app/src/main/java/com/github/alenastan/taskmannager/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.github.alenastan.taskmannager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.github.alenastan.taskmannager.utils.AuthUtils;
+
+/**
+ * Created by lena on 11.10.2014.
+ */
+public class LoginActivity extends ActionBarActivity {
+
+    EditText edit_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button cancelBtn = (Button) findViewById(R.id.cancel_btn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        edit_text = (EditText) findViewById(R.id.edit_text);
+    }
+
+    public void onLoginClick(View view) {
+        com.github.alenastan.taskmannager.utils.AuthUtils.setLogged(true);
+        setResult(RESULT_OK);
+        Intent intent = new Intent();
+        intent.putExtra("name",edit_text.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+
+}
--- TaskMannager/app/src/main/java/com/github/alenastan/taskmannager/MainActivity.java
+++ TaskMannager/app/src/main/java/com/github/alenastan/taskmannager/MainActivity.java
@@ -1,26 +1,30 @@
 package com.github.alenastan.taskmannager;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    TextView name_t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name_t = (TextView) findViewById(R.id.name_t);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onStartSampleClick(View view) {
+        startActivity(new Intent(this, SampleActivity.class));
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -34,3 +38,4 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
