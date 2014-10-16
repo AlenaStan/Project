147ea13e4aca8025bda014a471bf3210289b2641
Alena Stanchyts <lenka_stan@mail.ru>
2014-10-16 21:13:12 +0300

    added gridView

--- GridView/app/src/main/java/com/github/alenastan/gridview/MainActivity.java
+++ GridView/app/src/main/java/com/github/alenastan/gridview/MainActivity.java
@@ -2,35 +2,27 @@ package com.github.alenastan.gridview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.GridView;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Items> items = new ArrayList<Items>();
+        items.add(new Items ("Значение #1", 22));
+        items.add(new Items ("Значение #2", 66));
+        items.add(new Items("Значение #3", 89));
+        items.add(new Items ("Значение #4", 77));
+
+        gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new Adapter( this,items ));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
