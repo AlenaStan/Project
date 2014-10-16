5ed9edd21c24b1d0e769f9dade51dc8213bb31a5
Alena Stanchyts <lenka_stan@mail.ru>
2014-10-16 18:48:25 +0300

    added HW

--- TaskMannager/app/src/main/java/com/github/alenastan/taskmannager/LoginActivity.java
+++ TaskMannager/app/src/main/java/com/github/alenastan/taskmannager/LoginActivity.java
@@ -3,7 +3,6 @@ package com.github.alenastan.taskmannager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
