7f1318d6b6025730fea306d1c4bc3f56ebf01f59
Alena Stanchyts <lenka_stan@mail.ru>
2017-05-26 22:18:23 +0300

    deleting unused files.

--- ClientVk/app/src/main/java/com/github/alenastan/vkclient/fragments/FragmentNews.java
+++
@@ -1,9 +0,0 @@
-package com.github.alenastan.vkclient.fragments;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by lena on 19.01.2015.
- */
-public class FragmentNews extends Fragment {
-}
